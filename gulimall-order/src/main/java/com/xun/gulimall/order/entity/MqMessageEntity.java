1cb0083517a9edb6fe108a2ba512fadee5d13b2a
LiangXun <1251995355@qq.com>
2023-09-19 11:37:40 +0800

    gulimall初始化

---
+++ gulimall-order/src/main/java/com/xun/gulimall/order/entity/MqMessageEntity.java
@@ -0,0 +1,52 @@
+package com.xun.gulimall.order.entity;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * 
+ * 
+ * @author xun
+ * @email sunlightcs@gmail.com
+ * @date 2022-07-24 17:21:22
+ */
+@Data
+@TableName("mq_message")
+public class MqMessageEntity implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 
+	 */
+	@TableId
+	private String messageId;
+	/**
+	 * JSON
+	 */
+	private String content;
+	/**
+	 * 
+	 */
+	private String toExchange;
+	/**
+	 * 
+	 */
+	private String classType;
+	/**
+	 * 0-新建 1-已发送 2-错误抵达 3-已抵达
+	 */
+	private Integer messageStatus;
+	/**
+	 * 
+	 */
+	private Date createTime;
+	/**
+	 * 
+	 */
+	private Date updateTime;
+
+}
